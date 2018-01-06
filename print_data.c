3f35490826a560402c5f5c77abfcb228510e9368
Kyrylo Hrecheniuk <khrechen@e1r2p15.unit.ua>
2018-01-06 12:39:29 +0200

    nbr and width

--- print_data.c
+++ print_data.c
@@ -28,14 +28,37 @@ void	p_lower_case(void *data, t_specification spec)
 	ft_putendl("data");
 }
 
+/*
+** '0' - zero
+** '-' - minus
+** '+' - plus
+** ' ' - space
+*/
 void	di_lower_case(void *data, t_specification spec)
 {
 	int		nbr;
 	char	*str;
+	size_t	nbrlen;
 
 	nbr = (int)data;
 	str = ft_itoa(nbr);
-	ft_putstr(str);
+	nbrlen = ft_strlen(str) + (nbr < 0 ? 0 : 1);
+	spec.width = spec.width > nbrlen ? spec.width - (int)nbrlen : 0;
+	while (spec.width-- > 0)
+		ft_putchar(' ');
+	if (spec.flags.plus == true || spec.flags.space == true)
+	{
+		if (nbr >= 0)
+		{
+			ft_putchar((char)(spec.flags.plus == true ? '+' : ' '));
+			ft_putstr(str);
+		}
+		else
+			ft_putstr(str);
+	}
+	else
+		ft_putstr(str);
+	ft_strdel(&str);
 }
 
 void	d_upper_case(void *data, t_specification spec)
