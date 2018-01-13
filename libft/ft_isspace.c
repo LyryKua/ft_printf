c21a46530cdc5db32386a2ab996632528d18cd58
Kyrylo Hrecheniuk <khrechen@e1r3p13.unit.ua>
2018-01-13 12:45:10 +0200

    tabs

--- libft/ft_isspace.c
+++ libft/ft_isspace.c
@@ -12,8 +12,12 @@
 
 int		ft_isspace(int c)
 {
-	if (c == '\t' || c == '\n' || c == '\v' ||
-			c == '\f' || c == '\r' || c == ' ')
+	if (c == '\t' ||
+		c == '\n' ||
+		c == '\v' ||
+		c == '\f' ||
+		c == '\r' ||
+		c == ' ')
 		return (c);
 	return (0);
 }
