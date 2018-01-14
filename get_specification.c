d199d8f4b4adac0af2d7d9e39fb9ea7228e91952
Kyrylo <lyryk.ua@gmail.com>
2018-01-14 11:51:16 +0200

    get_spec

--- get_specification.c
+++ get_specification.c
@@ -13,7 +13,7 @@
 #include "inc/ft_printf.h"
 #include "libft.h"
 
-t_flag			get_flags(char *rep_spec)
+static t_flag	get_flags(char *rep_spec)
 {
 	t_flag	flags;
 
@@ -41,7 +41,7 @@ t_flag			get_flags(char *rep_spec)
 	return (flags);
 }
 
-int				get_width(char *replacing_spec)
+static int		get_width(char *replacing_spec)
 {
 	while ((*replacing_spec == '0' || !ft_isdigit(*replacing_spec)) &&
 																*replacing_spec)
@@ -49,25 +49,21 @@ int				get_width(char *replacing_spec)
 	return (*replacing_spec == '\0' ? 0 : ft_atoi(replacing_spec));
 }
 
-int				get_precision(char *replacing_spec, t_specification *spec)
+static int		get_precision(char *replacing_spec, t_flag *flags)
 {
-	while (*replacing_spec != '.' && *replacing_spec)
-		replacing_spec++;
-	if (*replacing_spec == '.')
-		replacing_spec++;
-	else
-		return (0);
-	if (*replacing_spec == '-')
-	{
-		spec->flags.minus = true;
-		spec->width = ft_atoi(++replacing_spec);
-		return (0);
-	}
-	spec->flags.zero = false;
-	return (ft_atoi(replacing_spec));
+	int	precision;
+	int	len;
+
+	len = (int)ft_strlen(replacing_spec);
+	while (replacing_spec[len - 1] != '.' && len > 0)
+		len--;
+	precision = len > 0 ? ft_atoi(replacing_spec + len) : 0;
+	if (precision != 0)
+		flags->zero = false;
+	return (precision);
 }
 
-char			*get_modifier(char *replacing_spec)
+static char		*get_modifier(char *replacing_spec)
 {
 	char	*modifier;
 
@@ -94,7 +90,7 @@ t_specification	get_specification(char *replacing_spec)
 
 	spec.flags = get_flags(replacing_spec);
 	spec.width = get_width(replacing_spec);
-	spec.precision = get_precision(replacing_spec, &spec);
+	spec.precision = get_precision(replacing_spec, &spec.flags);
 	spec.modifier = get_modifier(replacing_spec);
 	spec.type = replacing_spec[ft_strlen(replacing_spec) - 1];
 	spec.flags.zero = (spec.flags.zero && spec.flags.minus) ?
