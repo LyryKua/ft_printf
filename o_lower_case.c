351a0758771375ee036f99a1f73f2a0d0d2b6208
Kyrylo <lyryk.ua@gmail.com>
2018-01-14 17:25:49 +0200

    o and O

--- o_lower_case.c
+++ o_lower_case.c
@@ -21,6 +21,8 @@ static void	o_print(t_specification spec, char *str)
 		ft_putchar('0');
 		g_return++;
 	}
+	if (spec.flags.hash == true && spec.precision <= 0 && spec.width == 0)
+		return ;
 	while (spec.precision-- > (int)ft_strlen(str))
 	{
 		ft_putchar('0');
@@ -28,12 +30,35 @@ static void	o_print(t_specification spec, char *str)
 	}
 	ft_putstr(str);
 	g_return += ft_strlen(str);
+
 }
 
 static void	left_align(t_specification spec, char *str)
 {
+	int	width;
+	int	len;
+
+	if (spec.precision == -1 && !ft_strcmp(str, "0"))
+	{
+		if (spec.width > 0)
+		{
+			ft_putchar(' ');
+			g_return++;
+		}
+		return ;
+	}
 	o_print(spec, str);
-	ft_putstr("[left_align]");
+	len = (int)ft_strlen(str);
+	width = spec.width;
+	if (spec.flags.hash == true && spec.precision == 0)
+		width--;
+	if (spec.width > spec.precision && spec.flags.hash == true && len >= spec.precision && spec.precision != 0)
+		width--;
+	while (width-- > (spec.precision > len ? spec.precision : len))
+	{
+		ft_putchar(' ');
+		g_return++;
+	}
 }
 
 static void	right_align(t_specification spec, char *str)
@@ -44,6 +69,8 @@ static void	right_align(t_specification spec, char *str)
 	len = (int)ft_strlen(str);
 	width = spec.width;
 	if (spec.flags.hash == true && spec.precision == 0)
+		width--;
+	if (spec.width > spec.precision && spec.flags.hash == true && len >= spec.precision)
 		width--;
 	while (width-- > (spec.precision > len ? spec.precision : len))
 	{
@@ -64,8 +91,24 @@ static void	right_align(t_specification spec, char *str)
 
 static void	fill_zero(t_specification spec, char *str)
 {
-	ft_putstr("[fill_zero]");
+	int	len;
+	int	width;
+
+	len = (int)ft_strlen(str);
+	width = spec.width - (spec.precision > len ? spec.precision : len);
+	if (spec.flags.hash == true)
+	{
+		width--;
+		ft_putchar('0');
+		g_return++;
+	}
+	while (width-- > 0)
+	{
+		ft_putchar('0');
+		g_return++;
+	}
 	ft_putstr(str);
+	g_return += (int)ft_strlen(str);
 }
 
 void		o_lower_case(void *data, t_specification spec)
--- o_upper_case.c
+++ o_upper_case.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   o_upper_case.c                                     :+:      :+:    :+:   */
+/*   o_lower_case.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khrechen <khrechen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/09 17:03:00 by khrechen          #+#    #+#             */
-/*   Updated: 2018/01/09 17:03:00 by khrechen         ###   ########.fr       */
+/*   Created: 2018/01/09 16:53:00 by khrechen          #+#    #+#             */
+/*   Updated: 2018/01/09 16:53:00 by khrechen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ static void	o_print(t_specification spec, char *str)
 		ft_putchar('0');
 		g_return++;
 	}
+	if (spec.flags.hash == true && spec.precision <= 0 && spec.width == 0)
+		return ;
 	while (spec.precision-- > (int)ft_strlen(str))
 	{
 		ft_putchar('0');
@@ -28,6 +30,7 @@ static void	o_print(t_specification spec, char *str)
 	}
 	ft_putstr(str);
 	g_return += ft_strlen(str);
+
 }
 
 static void	left_align(t_specification spec, char *str)
@@ -35,7 +38,27 @@ static void	left_align(t_specification spec, char *str)
 	int	width;
 	int	len;
 
+	if (spec.precision == -1 && !ft_strcmp(str, "0"))
+	{
+		if (spec.width > 0)
+		{
+			ft_putchar(' ');
+			g_return++;
+		}
+		return ;
+	}
 	o_print(spec, str);
+	len = (int)ft_strlen(str);
+	width = spec.width;
+	if (spec.flags.hash == true && spec.precision == 0)
+		width--;
+	if (spec.width > spec.precision && spec.flags.hash == true && len >= spec.precision && spec.precision != 0)
+		width--;
+	while (width-- > (spec.precision > len ? spec.precision : len))
+	{
+		ft_putchar(' ');
+		g_return++;
+	}
 }
 
 static void	right_align(t_specification spec, char *str)
@@ -44,21 +67,48 @@ static void	right_align(t_specification spec, char *str)
 	int	len;
 
 	len = (int)ft_strlen(str);
-	width = spec.width - (spec.precision > len ? spec.precision : len);
-	if (spec.precision < 3)
+	width = spec.width;
+	if (spec.flags.hash == true && spec.precision == 0)
 		width--;
-	while (width-- > 0)
+	if (spec.width > spec.precision && spec.flags.hash == true && len >= spec.precision)
+		width--;
+	while (width-- > (spec.precision > len ? spec.precision : len))
 	{
 		ft_putchar(' ');
 		g_return++;
+	}
+	if (spec.precision == -1 && !ft_strcmp(str, "0"))
+	{
+		if (spec.width > 0)
+		{
+			ft_putchar(' ');
+			g_return++;
+		}
+		return ;
 	}
 	o_print(spec, str);
 }
 
 static void	fill_zero(t_specification spec, char *str)
 {
-	ft_putstr("[fill_zero]");
-	o_print(spec, str);
+	int	len;
+	int	width;
+
+	len = (int)ft_strlen(str);
+	width = spec.width - (spec.precision > len ? spec.precision : len);
+	if (spec.flags.hash == true)
+	{
+		width--;
+		ft_putchar('0');
+		g_return++;
+	}
+	while (width-- > 0)
+	{
+		ft_putchar('0');
+		g_return++;
+	}
+	ft_putstr(str);
+	g_return += (int)ft_strlen(str);
 }
 
 void		o_upper_case(void *data, t_specification spec)
@@ -67,7 +117,7 @@ void		o_upper_case(void *data, t_specification spec)
 	char			*str;
 
 	nbr = (unsigned long)data;
-	str = ft_uitoa_base(nbr, OCT);
+	str = ft_ultoa_base(nbr, OCT);
 	if (spec.flags.minus == true)
 		left_align(spec, str);
 	else if (spec.flags.zero == false)
