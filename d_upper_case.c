8766dd5850096b5c41becf24fafe1d36936dfe37
Kyrylo <lyryk.ua@gmail.com>
2018-01-14 13:11:38 +0200

    D

--- d_upper_case.c
+++ d_upper_case.c
@@ -5,26 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: khrechen <khrechen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/08 17:31:00 by khrechen          #+#    #+#             */
-/*   Updated: 2018/01/08 17:31:00 by khrechen         ###   ########.fr       */
+/*   Created: 2018/01/06 12:41:00 by khrechen          #+#    #+#             */
+/*   Updated: 2018/01/06 12:41:00 by khrechen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 #include "libft.h"
-
-/*
-** %D
-*/
+#include "ft_printf.h"
 
 static void	d_print(t_specification spec, char *str)
 {
-	if ((spec.flags.plus == true || spec.flags.space == true) && str[0] != '-')
+	if ((spec.flags.plus == true || spec.flags.space == true) && *str != '-')
 	{
 		ft_putchar((char)(spec.flags.plus == true ? '+' : ' '));
 		g_return++;
 	}
-	else if (str[0] == '-')
+	else if (*str == '-')
 	{
 		ft_putchar(*str++);
 		g_return++;
@@ -35,72 +31,91 @@ static void	d_print(t_specification spec, char *str)
 		g_return++;
 	}
 	ft_putstr(str);
-	g_return += ft_strlen(str);
+	g_return += (int)ft_strlen(str);
 }
 
 static void	left_align(t_specification spec, char *str)
 {
-	int	width;
 	int	len;
 
-	d_print(spec, str);
-	len = (int)ft_strlen(str);
-	width = spec.width - (spec.precision > len ? spec.precision : len);
-	if ((spec.flags.plus == true || spec.flags.space == true) && str[0] != '-')
-		width--;
-	if (str[0] == '-' && spec.precision - len >= 0)
-		width--;
-	while (width-- > 0)
+	if (spec.precision == -1 && !ft_strcmp(str, "0"))
+		while (spec.width-- > 0)
+		{
+			ft_putchar(' ');
+			g_return++;
+		}
+	else
 	{
-		ft_putchar(' ');
-		g_return++;
+		d_print(spec, str);
+		len = (int)ft_strlen(str);
+		if ((spec.flags.plus == true || spec.flags.space == true) &&
+			str[0] != '-')
+			spec.width--;
+		if (str[0] == '-' && spec.precision - len >= 0)
+			spec.width--;
+		while (spec.width-- > (spec.precision > len ? spec.precision : len))
+		{
+			ft_putchar(' ');
+			g_return++;
+		}
 	}
 }
 
 static void	right_align(t_specification spec, char *str)
 {
-	int	width;
 	int	len;
+	int	width;
 
 	len = (int)ft_strlen(str);
-	width = spec.width - (spec.precision > len ? spec.precision : len);
+	width = spec.width;
 	if ((spec.flags.plus == true || spec.flags.space == true) && str[0] != '-')
 		width--;
 	if (str[0] == '-' && spec.precision - len >= 0)
 		width--;
-	while (width-- > 0)
+	while (width-- > (spec.precision > len ? spec.precision : len))
 	{
 		ft_putchar(' ');
 		g_return++;
+	}
+	if (spec.precision == -1 && !ft_strcmp(str, "0"))
+	{
+		if (spec.width > 0)
+		{
+			ft_putchar(' ');
+			g_return++;
+		}
+		return ;
 	}
 	d_print(spec, str);
 }
 
 static void	fill_zero(t_specification spec, char *str)
 {
-	int	width;
 	int	len;
+	int	width;
 
 	len = (int)ft_strlen(str);
 	width = spec.width - (spec.precision > len ? spec.precision : len);
-	if (spec.flags.plus == true || spec.flags.space == true || str[0] == '-')
+	if ((spec.flags.plus == true || spec.flags.space == true) && *str != '-')
 	{
-		if (str[0] != '-')
-			ft_putchar((char)(spec.flags.plus == true ? '+' : ' '));
-		else
-		{
-			ft_putchar('-');
-			str++;
-		}
+		ft_putchar((char)(spec.flags.plus == true ? '+' : ' '));
+		width--;
 		g_return++;
 	}
+	else if (*str == '-')
+	{
+		ft_putchar(*str++);
+		g_return++;
+	}
+	if (str[0] == '-' && spec.precision - len >= 0)
+		width--;
 	while (width-- > 0)
 	{
 		ft_putchar('0');
 		g_return++;
 	}
 	ft_putstr(str);
-	g_return += ft_strlen(str);
+	g_return += (int)ft_strlen(str);
 }
 
 void		d_upper_case(void *data, t_specification spec)
