ff84fcd5677b6538fb439be26671da4385f500a4
Kyrylo <lyryk.ua@gmail.com>
2018-02-01 18:12:33 +0200

    a

--- main.c
+++ main.c
@@ -3,15 +3,15 @@
 
 int main(void)
 {
-	int	nbr = 42;
+//	int	nbr = 42;
 	int	or, ft;
 
-	or =    printf("   printf:\t|%*d|\n", 5, nbr);
-	ft = ft_printf("ft_printf:\t|%*d|\n", 5, nbr);
+	or =    printf("   printf:\t|%#5x|\n", 0);
+	ft = ft_printf("ft_printf:\t|%#5x|\n", 0);
 	printf("or = %d\tft = %d\n", or, ft);
 
-	or =    printf("   printf:\t|%#x|\n", 23);
-	ft = ft_printf("ft_printf:\t|%#x|\n", 23);
+	or =    printf("   printf:\t|%#25x|\n", 23);
+	ft = ft_printf("ft_printf:\t|%#25x|\n", 23);
 	printf("or = %d\tft = %d\n", or, ft);
 
 	ft = ft_printf("ft_printf:\t|%*b|\n", 10, 5);
