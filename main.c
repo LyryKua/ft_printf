6d35fa814a8c42dfce6c8c4f5181db086818d4cb
Kyrylo <lyryk.ua@gmail.com>
2018-02-09 17:28:03 +0200

    clean git

--- main.c
+++ main.c
@@ -9,5 +9,9 @@ int main(void)
 	or =    printf("   printf:\t|%d|\n", 42);
 	ft = ft_printf("ft_printf:\t|%d|\n", 42);
 	printf("or = %d\tft = %d\n\n", or, ft);
+
+	or =    printf("   printf:\t|%.18u|\n", INT_MAX + 1);
+	ft = ft_printf("ft_printf:\t|%.18u|\n", INT_MAX + 1);
+	printf("or = %d\tft = %d\n\n", or, ft);
 	return (0);
 }
