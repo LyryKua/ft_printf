0752ad5255b78017e633be69198952ed1fcbdc2b
Kyrylo <lyryk.ua@gmail.com>
2018-02-10 18:12:39 +0200

    hex

--- giv_me_correct_nbr.c
+++ giv_me_correct_nbr.c
@@ -34,23 +34,28 @@ long long			giv_me_correct_signed_nbr(void *data, char *modifier)
 	return (correct_nbr);
 }
 
-unsigned long long	giv_me_correct_unsigned_nbr(void *data, char *modifier)
+unsigned long long	giv_me_correct_unsigned_nbr(void *data, char *modifier, char type)
 {
 	unsigned long long	correct_nbr;
 
-	if (modifier == NULL)
-		correct_nbr = (unsigned int)data;
-	else if (!ft_strcmp(modifier, "hh"))
-		correct_nbr = (unsigned char)data;
-	else if (!ft_strcmp(modifier, "h"))
-		correct_nbr = (unsigned short)data;
-	else if (!ft_strcmp(modifier, "l"))
-		correct_nbr = (unsigned long)data;
-	else if (!ft_strcmp(modifier, "ll"))
-		correct_nbr = (unsigned long long)data;
-	else if (!ft_strcmp(modifier, "j"))
-		correct_nbr = (uintmax_t)data;
+	if (type != 'U' && type != 'D' && type != 'O')
+	{
+		if (modifier == NULL)
+			correct_nbr = (unsigned int)data;
+		else if (!ft_strcmp(modifier, "hh"))
+			correct_nbr = (unsigned char)data;
+		else if (!ft_strcmp(modifier, "h"))
+			correct_nbr = (unsigned short)data;
+		else if (!ft_strcmp(modifier, "l"))
+			correct_nbr = (unsigned long)data;
+		else if (!ft_strcmp(modifier, "ll"))
+			correct_nbr = (unsigned long long)data;
+		else if (!ft_strcmp(modifier, "j"))
+			correct_nbr = (uintmax_t)data;
+		else
+			correct_nbr = (size_t)data;
+	}
 	else
-		correct_nbr = (size_t)data;
+		correct_nbr = (unsigned long long)data;
 	return (correct_nbr);
 }
--- hex_unsigned_long_long.c
+++ hex_unsigned_long_long.c
@@ -15,21 +15,11 @@
 
 static void	hex_print(t_specification spec, char *str)
 {
-	if (spec.precision == -1)
-	{
-		spec.width != 0 ? ft_putchar(' ') : 0;
-		g_return += spec.width != 0 ? 1 : 0;
+	if (!ft_strcmp(str, "0") && spec.precision == -1)
 		return ;
-	}
-	if (!ft_strcmp(str, "0"))
+	if (spec.flags.hash == true && ft_strcmp(str, "0"))
 	{
-		ft_putchar('0');
-		g_return++;
-		return ;
-	}
-	if (spec.flags.hash == true)
-	{
-		ft_putstr(spec.type == 'x' ? "0x" : "0X");
+		ft_putstr(spec.type == 'X' ? "0X" : "0x");
 		g_return += 2;
 	}
 	while (spec.precision-- > (int)ft_strlen(str))
@@ -57,7 +47,8 @@ static void	left_align(t_specification spec, char *str)
 		&& spec.flags.zero == false
 		&& spec.flags.minus == false
 		&& spec.flags.plus == false
-		&& spec.flags.space == false)
+		&& spec.flags.space == false
+		&& spec.width != 0)
 		width += 2;
 	while (width-- > 0)
 	{
@@ -68,22 +59,26 @@ static void	left_align(t_specification spec, char *str)
 
 static void	right_align(t_specification spec, char *str)
 {
-	int	width;
-	int	len;
+	int width;
+	int len;
 
 	len = (int)ft_strlen(str);
-	width = spec.width
-			- (len > spec.precision ? len : spec.precision);
+	width = spec.width - (len > spec.precision ? len : spec.precision);
 	if (spec.flags.hash && len > spec.precision && spec.precision == 0)
 		width -= 2;
-	if (!ft_strcmp(str, "0")
-		&& spec.flags.hash
+	if (!ft_strcmp(str, "0") && spec.flags.hash
 		&& spec.flags.zero == false
 		&& spec.flags.minus == false
 		&& spec.flags.plus == false
-		&& spec.flags.space == false)
+		&& spec.flags.space == false
+		&& spec.width != 0)
 		width += 2;
 	while (width-- > 0)
+	{
+		ft_putchar(' ');
+		g_return++;
+	}
+	if (spec.precision == -1 && spec.width != 0)
 	{
 		ft_putchar(' ');
 		g_return++;
@@ -115,5 +110,26 @@ static void	fill_zero(t_specification spec, char *str)
 
 void		hex_unsigned_long_long(void *data, t_specification *spec)
 {
-	ft_putstr("[hex_unsigned_long_long]");
+	unsigned long long	nbr;
+	char				*str;
+	int					i;
+
+	nbr = giv_me_correct_unsigned_nbr(data, spec->modifier, spec->type);
+	str = ft_ulltoa_base(nbr, HEX);
+	if (spec->type == 'X')
+	{
+		i = 0;
+		while (str[i] != '\0')
+		{
+			str[i] = (char)ft_toupper(str[i]);
+			i++;
+		}
+	}
+	if (spec->flags.minus == true)
+		left_align(*spec, str);
+	else if (spec->flags.zero == false)
+		right_align(*spec, str);
+	else
+		fill_zero(*spec, str);
+	ft_strdel(&str);
 }
